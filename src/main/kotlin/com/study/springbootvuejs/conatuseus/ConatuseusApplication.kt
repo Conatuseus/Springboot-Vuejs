9b2ba076ea29f33e8eb80c7b0019e810b3b382c0
conatuseus <conatuseus@gmail.com>
2019-04-05 15:26:16 +0900

    Add vue.js

--- src/main/kotlin/com/study/springbootvuejs/conatuseus/ConatuseusApplication.kt
+++ src/main/kotlin/com/study/springbootvuejs/conatuseus/ConatuseusApplication.kt
@@ -1,9 +1,12 @@
 package com.study.springbootvuejs.conatuseus
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={ DataSourceAutoConfiguration.class})
 class ConatuseusApplication
 
 fun main(args: Array<String>) {
