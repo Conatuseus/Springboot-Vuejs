5f6f5b4c23b3270343f33765969db7e8b7e709b1
conatuseus <conatuseus@gmail.com>
2019-04-05 14:28:21 +0900

    First commit

---
+++ src/test/kotlin/com/study/springbootvuejs/conatuseus/ConatuseusApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.study.springbootvuejs.conatuseus
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ConatuseusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
